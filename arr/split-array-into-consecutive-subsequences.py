7abbe232b75565deeb19d1b7faf43bd04b3978cc
hanjie02 <hanjie02@58.com>
2020-12-04 22:10:39 +0800

    update

--- arr/split-array-into-consecutive-subsequences.py
+++ arr/split-array-into-consecutive-subsequences.py
@@ -1,14 +1,20 @@
 from collections import defaultdict
 import heapq
 
+# mp: key为nums数字，value为key对应的可以组成的子数组长度
+# queue：用于临时存储x-1的最小堆
+
+
 class Solution:
     def isPossible(self, nums: list) -> bool:
         mp = defaultdict(list)
         for x in nums:
             if queue := mp.get(x - 1):
+                # 每次取子数组长度最小值进行重组
                 prevLength = heapq.heappop(queue)
                 heapq.heappush(mp[x], prevLength + 1)
             else:
+                # 如果不存在x-1的数字，则新建数组
                 heapq.heappush(mp[x], 1)
         
         return not any(queue and queue[0] < 3 for queue in mp.values())
